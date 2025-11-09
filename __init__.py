a40b0556f646a0c443f3a05be6758e40ca021ab3
BunnyAI <119530153+WingeD123@users.noreply.github.com>
2025-11-09 08:40:07 +0800

    add string to bbox node to connect sam2

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from .qwen_25 import Qwen25Caption, Qwen25CaptionBatch
 from .qwen_3 import Qwen3Caption, Qwen3CaptionBatch
+from .string_to_bbox import StringToBbox
 
 # ----------------------------------------------------------------------------------
 # --- ComfyUI 映射 ---
@@ -12,6 +13,7 @@ NODE_CLASS_MAPPINGS = {
     "Qwen25CaptionBatch": Qwen25CaptionBatch,
     "Qwen3Caption": Qwen3Caption,
     "Qwen3CaptionBatch": Qwen3CaptionBatch,
+    "StringToBbox": StringToBbox
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -19,6 +21,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "Qwen25CaptionBatch": "Qwen2.5 VL Batch Caption",
     "Qwen3Caption": "Qwen3 VL Caption (Inverse Prompt)",
     "Qwen3CaptionBatch": "Qwen3 VL Batch Caption",
+    "StringToBbox": "String to BBOX"
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
